4b8ae5aec9cfc59a6ec7d84fdab621d6245da44e
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2021-03-30 09:11:59 -0400

    add username and email to context

--- server/lib/util.js
+++ server/lib/util.js
@@ -29,7 +29,8 @@ module.exports = {
     contextId: user.context_id || null,
     userId: user.user_id || null,
     userRole: getRole(user.roles),
-    // fullContext: user,
+    userEmail: user.lis_person_contact_email_primary,
+    userName: user.lis_person_name_full,
   }),
   parseQueryParameters: (headers) => {
     // match / or ? or both at the start of the location string, and remove.
